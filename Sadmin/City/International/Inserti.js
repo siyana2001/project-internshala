e0f74e0bf5875d44d7335b8c309f23ea016e454e
siyana2001 <142381847+siyana2001@users.noreply.github.com>
2024-03-05 14:10:37 +0530

    Add files via upload

---
+++ Sadmin/City/International/Inserti.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import 'react-bootstrap'; 
+
+export const Inserti = () => {
+  function inserting(event) {
+    event.preventDefault();
+
+    var image = document.getElementById("image").value;
+    var name = document.getElementById("name").value;
+    var companyname = document.getElementById("companyname").value;
+    var location = document.getElementById("location").value;
+    var experience = document.getElementById("experience").value;
+    var ctc = document.getElementById("ctc").value;
+    var applyby = document.getElementById("applyby").value;
+    var posted = document.getElementById("posted").value;
+    var applicants = document.getElementById("applicants").value;
+    var openings = document.getElementById("openings").value;
+    var keys = document.getElementById("keys").value;
+    var skills = document.getElementById("skills").value;
+    var aboutcompany = document.getElementById("aboutcompany").value;
+  
+    var data = {
+      "image": image,
+      "name": name,
+      "companyname": companyname,
+      "location": location,
+      "experience": experience,
+      "ctc": ctc,
+      "applyby": applyby,
+      "posted": posted,
+      "applicants": applicants,
+      "openings": openings,
+      "keys": keys,
+      "skills": skills,
+      "aboutcompany": aboutcompany
+    };
+
+    if (name === "") {
+      alert("Please enter the name");
+    } else if (companyname === "") {
+      alert("Please enter the company name");
+    } else if (location === "") {
+      alert("Please enter the location");
+    } else if (experience === "") {
+      alert("Please enter the experience");
+    } else if (ctc === "") {
+      alert("Please enter the CTC");
+    } else if (applyby === "") {
+      alert("Please enter the apply by date");
+    } else if (posted === "") {
+      alert("Please enter the posted date");
+    } else if (applicants === "") {
+      alert("Please enter the number of applicants");
+    } else if (openings === "") {
+      alert("Please enter the number of openings");
+    } else if (keys === "") {
+      alert("Please enter the keys");
+    } else if (skills === "") {
+      alert("Please enter the skills");
+    } else if (aboutcompany === "") {
+      alert("Please enter about the company");
+    } else {
+      axios.post("http://localhost:3009/insertinter", data)
+        .then((response, error) => {
+          if (error) {
+            alert("Data are not inserted");
+          } else if (response.data.status === "inserted") {
+            alert("Data are inserted successfully");
+            window.location.href = "/inters";
+          }
+        });
+    }
+  }
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <center>
+          <h3 className="mb-9">Insertion</h3>
+          <form onSubmit={inserting} style={{ border: '1px solid #ccc', width: '400px', padding: '20px' }}>
+            <div className="mb-3">
+              <input type='text' id='image' name='image' placeholder='Enter the URL' className='form-control' />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input type="text" className="form-control" id="name" name="name" required placeholder="Enter the name" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="companyname" className="form-label">Company Name</label>
+              <input type="text" className="form-control" id="companyname" name="companyname" placeholder="Enter company name" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="location" className="form-label">Location</label>
+              <input type="text" className="form-control" id="location" name="location" placeholder="Enter location" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="experience" className="form-label">Experience</label>
+              <input type="text" className="form-control" id="experience" name="experience" placeholder="Enter experience" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="ctc" className="form-label">CTC</label>
+              <input type="text" className="form-control" id="ctc" name="ctc" placeholder="Enter CTC" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="applyby" className="form-label">Apply By</label>
+              <input type="text" className="form-control" id="applyby" name="applyby" placeholder="Enter apply by date" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="posted" className="form-label">Posted</label>
+              <input type="text" className="form-control" id="posted" name="posted" placeholder="Enter posted date" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="applicants" className="form-label">Applicants</label>
+              <input type="text" className="form-control" id="applicants" name="applicants" placeholder="Enter number of applicants" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="openings" className="form-label">Openings</label>
+              <input type="text" className="form-control" id="openings" name="openings" placeholder="Enter number of openings" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="keys" className="form-label">About the job</label>
+              <input type="text" className="form-control" id="keys" name="keys" placeholder="Enter keys" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="skills" className="form-label">Skills</label>
+              <input type="text" className="form-control" id="skills" name="skills" placeholder="Enter skills required" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="aboutcompany" className="form-label">About Company</label>
+              <input type="text" className="form-control" id="aboutcompany" name="aboutcompany" placeholder="Enter about company" />
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <button className='enroll btn btn-dark' type="submit">Submit</button>
+              <Link to="/inters" className="enroll btn btn-dark">Back</Link>
+            </div>
+          </form>
+        </center>
+      </div>
+    </div>
+  );
+};
+
---
+++ Sadmin/City/Kolkata/Updatek.js
@@ -0,0 +1,176 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+export function Updatek() {
+
+    const { id } = useParams();
+    const [image, setImage] = useState('');
+    const [name, setName] = useState('');
+    const [companyname, setCompanyname] = useState('');
+    const [location, setLocation] = useState('');
+    const [experience, setExperience] = useState('');
+    const [ctc, setCtc] = useState('');
+    const [applyby, setApplyby] = useState('');
+    const [posted, setPosted] = useState('');
+    const [applicants, setApplicants] = useState('');
+    const [openings, setOpenings] = useState('');
+    const [keys, setKeys] = useState('');
+    const [skills, setSkills] = useState('');
+    const [aboutcompany, setAboutCompany] = useState('');
+
+    useEffect(() => {
+        fetch("http://localhost:3009/getupdatekolka/" + id)
+            .then(res => res.json())
+            .then((data) => {
+                setImage(data[0].image);
+                setName(data[0].name);
+                setCompanyname(data[0].companyname);
+                setLocation(data[0].location);
+                setExperience(data[0].experience);
+                setCtc(data[0].ctc);
+                setApplyby(data[0].applyby);
+                setPosted(data[0].posted);
+                setApplicants(data[0].applicants);
+                setOpenings(data[0].openings);
+                setKeys(data[0].keys);
+                setSkills(data[0].skills);
+                setAboutCompany(data[0].aboutcompany);
+               
+            });
+    }, []);
+    const updates = (event) => {
+        event.preventDefault();
+
+        var image = document.getElementById("image").value;
+        var name = document.getElementById("name").value;
+        var companyname = document.getElementById("companyname").value;
+        var location = document.getElementById("location").value;
+        var experience = document.getElementById("experience").value;
+        var ctc = document.getElementById("ctc").value;
+        var applyby = document.getElementById("applyby").value;
+        var posted = document.getElementById("posted").value;
+        var applicants = document.getElementById("applicants").value;
+        var openings = document.getElementById("openings").value;
+        var keys = document.getElementById("keys").value;
+        var skills = document.getElementById("skills").value;
+        var aboutcompany = document.getElementById("aboutcompany").value;
+
+
+        var data = {
+            "image":image,
+            "name":name,
+            "companyname":companyname,
+            "location":location,
+            "experience":experience,
+            "ctc":ctc,
+            "applyby":applyby,
+            "posted":posted,
+            "applicants":applicants,
+            "openings":openings,
+            "keys":keys,
+            "skills":skills,
+            "aboutcompany":aboutcompany,
+            
+        }
+        if (image === '') {
+            alert('URL is empty');
+        } else if (name === '') {
+            alert("Name is empty");
+        } else if (companyname === '') {
+            alert('Company name is empty');
+        } else if (location === '') {
+            alert('Location is empty');
+        } else if (experience === '') {
+            alert('Experience is empty');
+        } else if (ctc === '') {
+            alert('CTC is empty');
+        } else if (applyby === '') {
+            alert('Apply by is empty');
+        } else if (posted === '') {
+            alert('Posted is empty');
+        } else if (applicants === '') {
+            alert('Applicants is empty');
+        } else if (openings === '') {
+            alert('Openings is empty');
+        } else if (keys === '') {
+            alert('Keys is empty');
+        } else if (skills === '') {
+            alert('Skills is empty');
+        } else if (aboutcompany === '') {
+            alert('About Company is empty');
+        } else {
+            axios.put('http://localhost:3009/updatekolka/' + id, data)
+                .then((res) => {
+                    if (res.data.status === "error") {
+                        alert("Data are not updated");
+                    } else if (res.data.status === "updated") {
+                        alert("Data are updated successfully");
+                        window.location.href = "/kolkas";
+                    }
+                });
+        }
+    }
+
+  return (
+   <>
+  <div className="container">
+            <h1>Update</h1>
+            <form onSubmit={updates}>
+                <div className="mb-3">
+                    <label htmlFor="image" className="form-label">Image URL</label>
+                    <input type="text" className="form-control" id="image" value={image} onChange={(e) => setImage(e.target.value)} placeholder="Enter the URL" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter the Name" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="companyname" className="form-label">Company Name</label>
+                    <input type="text" className="form-control" id="companyname" value={companyname} onChange={(e) => setCompanyname(e.target.value)} placeholder="Enter the Company Name" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="location" className="form-label">Location</label>
+                    <input type="text" className="form-control" id="location" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Enter the Location" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="experience" className="form-label">Experience</label>
+                    <input type="text" className="form-control" id="experience" value={experience} onChange={(e) => setExperience(e.target.value)} placeholder="Enter the Experience" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="ctc" className="form-label">CTC</label>
+                    <input type="text" className="form-control" id="ctc" value={ctc} onChange={(e) => setCtc(e.target.value)} placeholder="Enter the CTC" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="applyby" className="form-label">Apply By</label>
+                    <input type="text" className="form-control" id="applyby" value={applyby} onChange={(e) => setApplyby(e.target.value)} placeholder="Enter the Apply By date" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="posted" className="form-label">Posted</label>
+                    <input type="text" className="form-control" id="posted" value={posted} onChange={(e) => setPosted(e.target.value)} placeholder="Enter the Posted days" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="applicants" className="form-label">Applicants</label>
+                    <input type="text" className="form-control" id="applicants" value={applicants} onChange={(e) => setApplicants(e.target.value)} placeholder="Enter the number of Applicants" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="openings" className="form-label">Openings</label>
+                    <input type="text" className="form-control" id="openings" value={openings} onChange={(e) => setOpenings(e.target.value)} placeholder="Enter the number of Openings" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="keys" className="form-label">Keys</label>
+                    <input type="text" className="form-control" id="keys" value={keys} onChange={(e) => setKeys(e.target.value)} placeholder="Enter the Keys" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="skills" className="form-label">Skills</label>
+                    <input type="text" className="form-control" id="skills" value={skills} onChange={(e) => setSkills(e.target.value)} placeholder="Enter the Skills" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="aboutcompany" className="form-label">About Company</label>
+                    <input type="text" className="form-control" id="aboutcompany" value={aboutcompany} onChange={(e) => setAboutCompany(e.target.value)} placeholder="Enter the About Company" />
+                </div>
+                <button type="submit" className="btn btn-primary">Update</button>
+            </form>
+        </div>
+   </>
+  )
+};
